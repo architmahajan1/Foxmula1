3bd4eeb2ea9b9f91c7f1f5b83870f3b5c8a6638f
architmahajan1 <47782203+architmahajan1@users.noreply.github.com>
2021-06-05 16:04:54 +0530

    Add files via upload

---
+++ assignment 3/PrimeOdd.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class CustomException extends Exception{
+    public CustomException(int num) {
+        System.out.println("No is invalid " +num);
+    }
+}
+public class PrimeOdd {
+
+    public static void main(String[] args) throws CustomException {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number for input");
+        int num = s.nextInt();
+        try {
+            if (num == 2) {
+                System.out.println("not a odd  number");
+            }
+            else
+            {
+                int count = 0;
+                for (int i = 2; i < num; i++) {
+                    if (num % i == 0) {
+                        count++;
+                    }
+                }
+                if (count > 0) {
+                    System.out.println("divisible by more than 2 number ");
+                }
+            }
+        }
+        catch (Exception e) {
+
+            throw new CustomException(num);
+        }
+    }
+}
+
+
+
